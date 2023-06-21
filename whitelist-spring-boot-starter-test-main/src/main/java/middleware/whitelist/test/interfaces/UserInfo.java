c548eaae7d2737a522ae1f1ee184335c3a6e19bd
sleep-falcon <50351371+sleep-falcon@users.noreply.github.com>
2023-06-20 21:58:12 -0700

    Update UserInfo.java

--- whitelist-spring-boot-starter-test-main/src/main/java/middleware/whitelist/test/interfaces/UserInfo.java
+++ whitelist-spring-boot-starter-test-main/src/main/java/middleware/whitelist/test/interfaces/UserInfo.java
@@ -1,10 +1,6 @@
 package middleware.whitelist.test.interfaces;
 
-/**
- * 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
- * 公众号：bugstack虫洞栈
- * Create by 小傅哥(fustack)
- */
+
 class UserInfo {
 
     //code、info可以统一定义一个类
